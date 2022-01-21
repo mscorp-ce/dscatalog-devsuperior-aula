88bde5ba860a3ac1510a28a82d68b166832c3f3f
mscorp-ce <m.s.copr@gmail.com>
2022-01-20 22:51:39 -0300

    H2 database

---
+++ backend/src/main/java/com/devsuperior/dscatalog/model/servicies/CategoryService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dscatalog.model.servicies;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.model.entities.Category;
+import com.devsuperior.dscatalog.model.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		
+		return repository.findAll();
+	}
+
+}
